60f548073bfdfc417c0a78cf1f9296ee7c9b4d5c
Philip Lundrigan <philiplundrigan@gmail.com>
2015-07-09 11:27:26 -0400

    Build server to display images and handle deletes

--- show_dups.py
+++ show_dups.py
@@ -2,6 +2,9 @@
 
 import pickle
 from jinja2 import Template, FileSystemLoader, Environment
+from flask import Flask
+
+app = Flask(__name__)
 
 def load_hash(name):
     with open(name, 'rb') as f:
@@ -20,12 +23,24 @@ def find_dups(*datas):
 
     return duplicates
 
-ios_hashes = load_hash('ios_pictures.pickle')
-bridget_hashes = load_hash('bridget_pictures.pickle')
-dups = find_dups(ios_hashes, bridget_hashes)
+@app.route('/')
+def show_dups():
+    ios_hashes = load_hash('ios_pictures.pickle')
+    bridget_hashes = load_hash('bridget_pictures.pickle')
+    dups = find_dups(ios_hashes, bridget_hashes)
 
-env = Environment(loader=FileSystemLoader('template'))
-template = env.get_template('index.html')
+    dups = dups[:1]
 
-with open('output/index.html', 'w') as f:
-    f.write(template.render(duplicates=dups))
+    env = Environment(loader=FileSystemLoader('template'))
+    template = env.get_template('index.html')
+
+    return template.render(duplicates=dups)
+
+@app.route('/picture/<path:file_name>', methods=['DELETE'])
+def delete_picture(file_name):
+    file_name = "/" + file_name
+    print(file_name)
+    return "True"
+
+if __name__ == '__main__':
+    app.run(debug=True)
