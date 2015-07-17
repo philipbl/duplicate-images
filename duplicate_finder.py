4f6e60d38af56d1a369805411bd9cbacde57ba6e
Philip Lundrigan <philiplundrigan@gmail.com>
2015-07-17 10:00:41 -0400

    Recursively search for images and skip already hashed

--- duplicate_finder.py
+++ duplicate_finder.py
@@ -3,7 +3,7 @@
 Uses phash to find duplicate pictures.
 
 Usage:
-    duplicate_finder.py add <path> [--ignore | --error] [--database=<db_file>]
+    duplicate_finder.py add <path> [--database=<db_file>]
     duplicate_finder.py remove <path> [--database=<db_file>]
     duplicate_finder.py clear [--database=<db_file>]
     duplicate_finder.py show [--database=<db_file>]
@@ -12,8 +12,9 @@ Usage:
 
 Options:
     -h, -–help      Show this screen
-    --ignore        Ignore an image file if it has already been hashed (default)
-    --errors        Display an error if an image file has already been hashed,
+    --skip          Check if a hash already exists for file and if so don't regenerate
+                    hash (default)
+    --error         Display an error if an image file has already been hashed,
                     but continuing calculating hashes for other images
     --print         Only print duplicate files
     --trash         Where files will be put when they are deleted (default: ./Trash)
@@ -31,7 +32,6 @@ from termcolor import colored, cprint
 from contextlib import contextmanager
 from pymongo import MongoClient
 from glob import glob
-from enum import Enum
 from multiprocessing import Pool, Value
 from functools import partial
 from pprint import pprint
@@ -39,22 +39,17 @@ from tempfile import NamedTemporaryFile
 from jinja2 import Template, FileSystemLoader, Environment
 from flask import Flask, send_from_directory
 from PIL import Image, ExifTags
+from subprocess import Popen
 import shutil
 
 TRASH = "./Trash/"
 
-class DuplicateType(Enum):
-    ignore = 1
-    error = 2
 
 DEFAULT_DATABASE = "hashes.db"
-DUPLICATE_TYPE = DuplicateType.ignore
 
 @contextmanager
 def connect_to_db():
-    # Start database
-    # mongod --config /usr/local/etc/mongod.conf
-
+    p = Popen(['mongod', '--config', 'mongod.conf'])
     client = MongoClient()
     db = client.image_database
     images = db.images
@@ -62,45 +57,52 @@ def connect_to_db():
     yield images
 
     client.close()
-    # Stop database
+    p.kill()
 
 def get_image_files(path):
-    # TODO: Add more file types
-    files = glob(path + '/*.jpg') + \
-            glob(path + '/*.JPG') + \
-            glob(path + '/*.png') + \
-            glob(path + '/*.PNG')
+    def is_image(file_name):
+        file_name = file_name.lower()
+        return file_name.endswith('.jpg') or  \
+               file_name.endswith('.jpeg') or \
+               file_name.endswith('.png') or  \
+               file_name.endswith('.gif') or  \
+               file_name.endswith('.tiff')
 
-    return files
+    path = os.path.abspath(path)
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if is_image(file):
+                yield os.path.join(root, file)
 
-def hash_file(file, cb):
-    cb(file, str(imagehash.phash(Image.open(file))))
-    cprint("\tHashed {}".format(file), "blue")
+def hash_file(file, contains_cb, result_cb):
+    if contains_cb(file):
+        cprint("\tSkipping {}".format(file), "green")
+    else:
+        hash_ = str(imagehash.phash(Image.open(file)))
+        result_cb(file, hash_)
+        cprint("\tHashed {}".format(file), "blue")
 
-def hash_files_single(files, cb):
-    for file in files:
-        hash_file(file, cb)
-
-def hash_files_parallel(files, cb):
+def hash_files_parallel(files, contains_cb, result_cb):
     with Pool(8) as p:
-        func = partial(hash_file, cb=cb)
+        func = partial(hash_file,
+                       contains_cb=contains_cb,
+                       result_cb=result_cb)
         p.map(func, files)
 
 def _add_to_database(file, hash):
     try:
         db.insert_one({"_id": file, "hash": hash})
     except pymongo.errors.DuplicateKeyError:
-        if DUPLICATE_TYPE == DuplicateType.ignore:
-            pass
-        elif DUPLICATE_TYPE == DuplicateType.error:
-            cprint("Duplicate key: {}".format(file), "red")
+        cprint("Duplicate key: {}".format(file), "red")
+
+def _in_database(file):
+    return db.count({"_id": file}) > 0
 
 def add(path, db):
     cprint("Hashing {}".format(path), "blue")
     files = get_image_files(path)
 
-    # hash_files_single(files, _add_to_database)
-    hash_files_parallel(files, _add_to_database)
+    hash_files_parallel(files, _in_database, _add_to_database)
 
     cprint("...done", "blue")
 
@@ -211,11 +213,6 @@ if __name__ == '__main__':
 
     if database is None:
         database = DEFAULT_DATABASE
-
-    if args['--ignore']:
-        DUPLICATE_TYPE = DuplicateType.ignore
-    elif args['--error']:
-        DUPLICATE_TYPE = DuplicateType.error
 
     with connect_to_db() as db:
         if args['add']:
