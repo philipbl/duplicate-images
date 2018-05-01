9c86051df20ea97ef2bddd4cbc8bb5bb8a3a491a
Marc Richter <marc.richter@docomodigital.com>
2018-05-01 12:39:22 +0200

    Removed psutil dependency, since concurrent.futures is capable of this already on it's own.

--- duplicate_finder.py
+++ duplicate_finder.py
@@ -36,7 +36,6 @@ import os
 import magic
 import math
 from pprint import pprint
-import psutil
 import shutil
 from subprocess import Popen, PIPE, TimeoutExpired
 from tempfile import TemporaryDirectory
@@ -50,11 +49,6 @@ from more_itertools import chunked
 from PIL import Image, ExifTags
 import pymongo
 from termcolor import cprint
-
-
-TRASH = "./Trash/"
-DB_PATH = "./db"
-NUM_PROCESSES = psutil.cpu_count()
 
 
 @contextmanager
@@ -348,9 +342,13 @@ if __name__ == '__main__':
 
     if args['--trash']:
         TRASH = args['--trash']
+    else:
+        TRASH = "./Trash/"
 
     if args['--db']:
         DB_PATH = args['--db']
+    else:
+        DB_PATH = "./db"
 
     if args['--db-name']:
         DB_NAME = args['--db-name']
@@ -364,6 +362,8 @@ if __name__ == '__main__':
 
     if args['--parallel']:
         NUM_PROCESSES = int(args['--parallel'])
+    else:
+        NUM_PROCESSES = None
 
     with connect_to_db(db_conn_string=DB_PATH, db_name=DB_NAME, db_coll=DB_COLL) as db:
         if args['add']:
