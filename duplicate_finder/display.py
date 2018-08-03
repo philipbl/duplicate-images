e96b4803ac75fda964ea02c978e97c3dca1c5d66
Philip Lundrigan <philiplundrigan@gmail.com>
2018-08-03 14:22:20 +0800

    Fix display

--- duplicate_finder/display.py
+++ duplicate_finder/display.py
@@ -1,7 +1,8 @@
 import json
+import math
 import webbrowser
 
-from flask import Flask, send_from_directory, send_file
+from flask import Flask, request, send_from_directory, send_file
 from flask_cors import CORS
 from jinja2 import FileSystemLoader, Environment
 from werkzeug.routing import PathConverter
@@ -15,12 +16,27 @@ CORS(app)
 app.url_map.converters['everything'] = EverythingConverter
 
 
-def display_duplicates(duplicates, delete_cb):
+def display_duplicates(duplicates, delete_cb, duplicates_per_page=10):
     @app.route('/')
     def main_page():
         env = Environment(loader=FileSystemLoader('template'))
         template = env.get_template('index.html')
-        return template.render(duplicates=json.dumps(duplicates))
+
+        current = int(request.args.get('current', 0))
+        total = math.ceil(len(duplicates) / duplicates_per_page)
+
+        # Validate current
+        if current < 0:
+            current = 0
+        if current >= total:
+            current = total - 1
+
+        start = current * duplicates_per_page
+        end = (current + 1) * duplicates_per_page
+
+        return template.render(duplicates=duplicates[start:end],
+                               current=current,
+                               total=total)
 
     @app.route('/picture/<everything:file_name>', methods=['GET'])
     def get_picture(file_name):
